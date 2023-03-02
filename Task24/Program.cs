92c5db5b9ce45fd507f5b238f911a02689f875c7
woxydster <woxydster@gmail.com>
2023-03-02 17:01:16 +0300

    Task comleted

---
+++ Task24/Program.cs
@@ -0,0 +1,30 @@
+﻿// 24. Напишите программу, которая принимает на вход число (А)
+// и выдаёт сумму чисел от 1 до А
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int number = Prompt("Введите натуральное число ");
+
+int Prompt(string message)                  // Метод ввода числа int
+{
+    Console.Write(message);
+    int readNumber = Convert.ToInt32(Console.ReadLine());
+    return readNumber;
+}
+
+
+int sum = Sum(number);
+
+int Sum(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+Console.WriteLine($"Сумма чисел от 1 до {number} равна {sum}");
