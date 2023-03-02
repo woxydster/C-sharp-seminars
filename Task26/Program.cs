8a216ffb47b613476c34dc9a2d515f85c4019874
woxydster <woxydster@gmail.com>
2023-03-02 17:02:49 +0300

    Task completed

---
+++ Task26/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число
+// и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int number = Prompt("Введите натуральное число ");
+
+int Prompt(string message)                  // Метод ввода числа int
+{
+    Console.Write(message);
+    int readNumber = Convert.ToInt32(Console.ReadLine());
+    return readNumber;
+}
+
+int numberOfDigits = NumberOfDigits(number);
+
+int NumberOfDigits(int num)
+{
+    int numberOfDigits = 0;
+    while (num != 0)
+    {
+        num = num / 10;
+        numberOfDigits = numberOfDigits + 1;
+    }
+    return numberOfDigits;
+}
+
+Console.WriteLine($"Количество цифр в числе равно {numberOfDigits}");
