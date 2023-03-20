3d00be75fe1a89b901b6408344c0844e0f865549
woxydster <woxydster@gmail.com>
2023-03-20 21:57:35 +0300

    Task completed

---
+++ Task44/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 44. Не используя рекурсию, выведите первые N чисел Фибоначчи.
+// При N = 5 -> 00123
+
+int userNumber = Prompt("Введите натуральное число  ");
+
+int Prompt(string message)                  // 
+{
+    Console.Write(message);
+    int readNumber = Convert.ToInt32(Console.ReadLine());
+    return readNumber;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write($"N = {userNumber} -> ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}");
+    }
+}
+
+int[] FibonacciArray(int num)
+{    
+    int[] array = new int [num];
+    array[0] = 0;
+    array[1] = 1;
+        for (int i = 2; i < num; i++)
+        {
+            array[i]= array[i-2]+array[i-1];
+        }
+    return array;
+}
+
+int[] fibonacci = FibonacciArray(userNumber);
+PrintArray(fibonacci);
